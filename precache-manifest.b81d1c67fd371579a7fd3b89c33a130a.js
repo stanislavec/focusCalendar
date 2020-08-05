e1163ef107bb6fa087df33296e232bb414f35a92
Stanislav Kryzhanovsky <stanislavkryzhanovsky@gmail.com>
2020-08-05 15:49:19 +0300

    Updates

---
+++ precache-manifest.b81d1c67fd371579a7fd3b89c33a130a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2de140faa256dccd99b65c65e831a825",
+    "url": "/focusCalendar/index.html"
+  },
+  {
+    "revision": "0519d5957551e4129aa9",
+    "url": "/focusCalendar/static/css/main.45afa9c8.chunk.css"
+  },
+  {
+    "revision": "9ba8244f8c3bf2e2992b",
+    "url": "/focusCalendar/static/js/2.d2ff16b2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/focusCalendar/static/js/2.d2ff16b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0519d5957551e4129aa9",
+    "url": "/focusCalendar/static/js/main.23bd2926.chunk.js"
+  },
+  {
+    "revision": "ced65c99448e5d4dc6f1",
+    "url": "/focusCalendar/static/js/runtime-main.a90ae171.js"
+  }
+]);

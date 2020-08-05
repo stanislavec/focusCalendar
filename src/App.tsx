38c52402d9f111c7d79f5f08096c5722bb979ef9
Stanislav Kryzhanovsky <stanislavkryzhanovsky@gmail.com>
2020-08-05 14:57:08 +0300

    Написал компонент AutoSizeInput

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
 import React from 'react';
 import { AutoSizeInput } from './components';
+import './App.css';
 
 function App() {
+  const nameOfMeeting = React.createRef();
+  const location = React.createRef();
+  const agenda = React.createRef();
   return (
     <div className="App">
-      <AutoSizeInput />
+      <AutoSizeInput
+        autoFocus
+        tabIndex="1"
+        ref={nameOfMeeting}
+        label="Name of Meeting"
+      />
+      <AutoSizeInput tabIndex="2" ref={location} label="Location / Call" />
+      <AutoSizeInput
+        tabIndex="3"
+        ref={agenda}
+        defaultValue="Значение по умолчанию"
+        label="Agenda"
+      />
     </div>
   );
 }
--- src/components/AutoSizeInput.tsx
+++ src/components/AutoSizeInput.tsx
@@ -1,6 +1,133 @@
-import React from 'react';
+import React, { useState, forwardRef, useEffect } from 'react';
+import styled from '@emotion/styled';
+import { useSpring, animated } from 'react-spring';
 import TextareaAutosize from 'react-textarea-autosize';
 
-export const AutoSizeInput = () => {
-  return <TextareaAutosize />;
-};
+interface IAutoSizeInputProps {
+  tabIndex?: string;
+  ref?: any;
+  label?: string;
+  defaultValue?: string;
+  value?: string;
+  autoFocus?: boolean;
+  onChange?: (value: string) => void;
+  onFocus?: () => void;
+  onClick?: () => void;
+  onBlur?: () => void;
+}
+
+//@ts-ignore
+const StyledLabel = styled(animated.label)`
+  position: absolute;
+  height: 28px;
+  left: 10px;
+  opacity: 0.6;
+  color: white;
+  z-index: -1;
+`;
+
+const StyledTextareaAutosize = styled(TextareaAutosize)(
+  // @ts-ignore
+  ({ isLongContent }) => ({
+    width: '100%',
+    height: '35px !important',
+    padding: '5px',
+    background: 'transparent',
+    fontSize: isLongContent ? '14px' : '22px',
+
+    border: '2px solid transparent',
+    borderBottom: '2px solid white',
+    color: 'white',
+
+    resize: 'none',
+    outline: 'none',
+  })
+);
+
+const AutoSizeInputWrapper = styled.div`
+  width: 350px;
+  height: 75px;
+
+  position: relative;
+  padding: 10px 10px 20px 10px;
+  z-index: 0;
+`;
+
+export const AutoSizeInput: React.FC<IAutoSizeInputProps> = forwardRef(
+  (
+    {
+      defaultValue,
+      value,
+      label,
+      tabIndex,
+      autoFocus,
+      onFocus,
+      onClick,
+      onBlur,
+      onChange,
+    },
+    ref
+  ) => {
+    const textAreaProps = { value, defaultValue, autoFocus, tabIndex };
+    const [areaValue, setAreaValue] = useState(value);
+    const [isFocused, setIsFocused] = useState(autoFocus);
+    const [wasChanged, setWasChanged] = useState(false);
+    const [isLongContent, setIsLongContent] = useState(false);
+
+    useEffect(() => {
+      if (value) setIsLongContent(value.length >= 40);
+      if (!value && defaultValue) setIsLongContent(defaultValue.length >= 40);
+      // eslint-disable-next-line
+    }, []);
+
+    const onAreaFocus = () => {
+      setIsFocused(true);
+      if (onFocus) onFocus();
+    };
+
+    const onAreaBlur = () => {
+      setIsFocused(false);
+      if (onBlur) onBlur();
+    };
+
+    const onAreaChange = (event: any) => {
+      setIsLongContent(event.target.value.length >= 40);
+      setAreaValue(event.target.value);
+      setWasChanged(true);
+      if (onChange) {
+        onChange(event.target.value);
+      }
+    };
+
+    const manageKeyboardEvents = (keyCode: number) => {
+      if (keyCode === 27) {
+        (ref as any).current.blur();
+      }
+    };
+
+    const animProps = useSpring({
+      config: { duration: 150 },
+      top: isFocused ? '55px' : '10px',
+      fontSize: isFocused ? '14px' : '22px',
+    });
+
+    return (
+      <AutoSizeInputWrapper>
+        {label &&
+          ((!wasChanged && !defaultValue) || (wasChanged && !areaValue)) && (
+            <StyledLabel style={animProps}>{label}</StyledLabel>
+          )}
+        <StyledTextareaAutosize
+          onFocus={() => onAreaFocus()}
+          onBlur={() => onAreaBlur()}
+          {...textAreaProps}
+          onChange={onAreaChange}
+          onKeyUp={(e) => manageKeyboardEvents(e.keyCode)}
+          // @ts-ignore
+          isLongContent={isLongContent}
+          ref={(tag) => ((ref as any).current = tag)}
+        />
+      </AutoSizeInputWrapper>
+    );
+  }
+);
